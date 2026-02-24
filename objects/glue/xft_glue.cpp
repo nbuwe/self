4452c904411a22ddbcc7446dd8485560efeeebc4
Valery Ushakov <uwe@stderr.spb.ru>
2026-02-25 01:43:24 +0300

    glue/xft: add a few more libXft wrappers

--- objects/glue/xft_glue.cpp
+++ objects/glue/xft_glue.cpp
@@ -36,13 +36,67 @@ xTypeSealsDo(declareXTypeSeals)
 
 
 
+Bool
+XftColorAllocValueRGBA_wrap(Display *display,
+			    Visual *visual,
+			    Colormap cmap,
+			    unsigned short red,
+			    unsigned short green,
+			    unsigned short blue,
+			    unsigned short alpha,
+			    XftColor *result)
+{
+    XRenderColor rc;
+    rc.red = red;
+    rc.green = green;
+    rc.blue = blue;
+    rc.alpha = alpha;
+
+    Bool ok = XftColorAllocValue(display, visual, cmap, &rc, result);
+    return ok;
+}
+
+
+char *
+XftFontPatternFormat_wrap(XftFont *font, char *format)
+{
+    if (font == NULL || format == NULL)
+	return NULL;
+
+    // allocs result for the caller to manage
+    return (char *)FcPatternFormat(font->pattern, (FcChar8 *)format);
+}
+
+
 void
 XftTextExtents8_wrap(Display *display, XftFont *font,
 		     char *string, int len,
 		     XGlyphInfo *extents)
 {
-  FcChar8 *xstr = (FcChar8 *)string;
-  XftTextExtents8(display, font, (FcChar8*)string, len, extents);
+    FcChar8 *xstr = (FcChar8 *)string;
+    XftTextExtents8(display, font, (FcChar8*)string, len, extents);
+}
+
+
+void
+XftDrawSetNoClipMask_wrap(XftDraw *draw)
+{
+    XftDrawSetClip(draw, NULL);
+}
+
+
+void
+XftDrawSetClipRectangle_wrap(XftDraw *draw,
+			     int  x,    int y,
+			     int width, int height)
+{
+    XRectangle rect;
+    rect.x = x;
+    rect.y = y;
+    rect.width = width;
+    rect.height = height;
+
+    XftDrawSetClipRectangles(draw, 0, 0, &rect, 1);
 }
 
 
@@ -55,7 +109,7 @@ XftDrawString8_wrap(XftDraw  *draw,
 		    char     *string,
 		    int       len)
 {
-    XftDrawString8(draw, color, pub, x, y, (FcChar8*)string, len);
+    XftDrawString8(draw, color, pub, x, y, (FcChar8 *)string, len);
 }
 
 
